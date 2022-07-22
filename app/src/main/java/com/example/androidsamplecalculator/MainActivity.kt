258ffde2606f389ff1f8a5d92530726fbbee042a
0si43 <symn43@gmail.com>
2022-07-22 20:43:18 +0900

    add calculator function

--- app/src/main/java/com/example/androidsamplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/androidsamplecalculator/MainActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.remember
 import androidx.compose.material3.*
-import androidx.compose.*
-import androidx.compose.ui.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidsamplecalculator.ui.theme.AndroidSampleCalculatorTheme
@@ -22,20 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column() {
-                        ButtonRow(
-                            "7", "8","9","*",
-                        )
-                        ButtonRow(
-                            "4", "5","6","-",
-                        )
-                        ButtonRow(
-                            "1", "2","3","+",
-                        )
-                        ButtonRow(
-                            "0", ".","x","=",
-                        )
-                    }
+                    AppMain()
                 }
             }
         }
@@ -43,20 +29,123 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ButtonRow(first: String, second: String, third: String, fourth: String) {
-    Row () {
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = first)
+fun AppMain() {
+    Column() {
+        var result by remember { mutableStateOf("") }
+        val onTapNumber: (Int) -> Unit = {
+            if (result != "" || it != 0) {
+                result += it.toString()
+            }
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = second)
+        var savedNumber by remember { mutableStateOf("") }
+        var operator by remember { mutableStateOf("") }
+        val onTapOperator: (String) -> Unit = {
+            if (savedNumber.isEmpty()) {
+                savedNumber = result
+                result = ""
+                operator = it
+            }
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = third)
+        val onTapEqual: () -> Unit = {
+            if (!savedNumber.isEmpty()) {
+                val lhs = savedNumber.toIntOrNull() ?: 0
+                val rhs = result.toIntOrNull() ?: 0
+                when (operator) {
+                    "+" -> result = (lhs + rhs).toString()
+                    "-" -> result = (lhs - rhs).toString()
+                    "*" -> result = (lhs * rhs).toString()
+                    "/" -> result = (lhs / rhs).toString()
+                }
+                savedNumber = ""
+                operator = ""
+            }
         }
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = fourth)
+
+        val onTapDelete: () -> Unit = {
+            if (!result.isEmpty()) {
+                result = result.dropLast(1)
+            }
         }
+
+        val onTapClear: () -> Unit = {
+            if (!result.isEmpty()) {
+                result = ""
+            }
+        }
+
+        Text(text = result.toString())
+        Row() {
+            ClearButton(onTap = onTapClear)
+            EmptyButton()
+            EmptyButton()
+            OperatorButton(operator = "/", onTap = onTapOperator)
+        }
+        Row() {
+            NumberButton(number = 7, onTap = onTapNumber)
+            NumberButton(number = 8, onTap = onTapNumber)
+            NumberButton(number = 9, onTap = onTapNumber)
+            OperatorButton(operator = "*", onTap = onTapOperator)
+        }
+        Row() {
+            NumberButton(number = 4, onTap = onTapNumber)
+            NumberButton(number = 5, onTap = onTapNumber)
+            NumberButton(number = 6, onTap = onTapNumber)
+            OperatorButton(operator = "-", onTap = onTapOperator)
+        }
+        Row() {
+            NumberButton(number = 1, onTap = onTapNumber)
+            NumberButton(number = 2, onTap = onTapNumber)
+            NumberButton(number = 3, onTap = onTapNumber)
+            OperatorButton(operator = "+", onTap = onTapOperator)
+        }
+        Row() {
+            NumberButton(number = 0, onTap = onTapNumber)
+            EmptyButton()
+            DeleteButton(onTap = onTapDelete)
+            EqualButton(onTap = onTapEqual)
+        }
+    }
+}
+
+@Composable
+fun NumberButton(number: Int, onTap: (Int) -> Unit) {
+    Button(onClick = { onTap(number) } ) {
+        Text(text = number.toString())
+    }
+}
+
+@Composable
+fun EmptyButton() {
+    Button(onClick = { }) {
+        Text(text = "")
+    }
+}
+
+@Composable
+fun OperatorButton(operator: String, onTap: (String) -> Unit) {
+    Button(onClick = { onTap(operator) }) {
+        Text(text = operator)
+    }
+}
+
+@Composable
+fun EqualButton(onTap: () -> Unit) {
+    Button(onClick = onTap) {
+        Text(text = "=")
+    }
+}
+
+@Composable
+fun ClearButton(onTap: () -> Unit) {
+    Button(onClick = onTap) {
+        Text(text = "C")
+    }
+}
+
+@Composable
+fun DeleteButton(onTap: () -> Unit) {
+    Button(onClick = onTap) {
+        Text(text = "D")
     }
 }
 
@@ -64,19 +153,6 @@ fun ButtonRow(first: String, second: String, third: String, fourth: String) {
 @Composable
 fun DefaultPreview() {
     AndroidSampleCalculatorTheme {
-        Column() {
-            ButtonRow(
-                "7", "8","9","*",
-            )
-            ButtonRow(
-                "4", "5","6","-",
-            )
-            ButtonRow(
-                "1", "2","3","+",
-            )
-            ButtonRow(
-                "0", ".","x","=",
-            )
-        }
+        AppMain()
     }
 }
