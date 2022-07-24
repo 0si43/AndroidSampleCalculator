0a3938b9f1ca55f0f393fb66b2eaef6dac2fddfa
0si43 <symn43@gmail.com>
2022-07-24 13:38:23 +0900

    divided files

--- app/src/main/java/com/example/androidsamplecalculator/AppMain.kt
+++ app/src/main/java/com/example/androidsamplecalculator/AppMain.kt
@@ -5,32 +5,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 
-enum class Operator(val symbol: String) {
-    PLUS("+") {
-        override fun calculate(lhs: Int, rhs: Int): Int {
-            return lhs + rhs
-        }
-    },
-    MINUS("-"){
-        override fun calculate(lhs: Int, rhs: Int): Int {
-            return lhs - rhs
-        }
-    },
-    MULTIPLY("*"){
-        override fun calculate(lhs: Int, rhs: Int): Int {
-            return lhs * rhs
-        }
-    },
-    DIVIDE("/"){
-        override fun calculate(lhs: Int, rhs: Int): Int {
-            return lhs / rhs
-        }
-    };
-
-    abstract fun calculate(lhs: Int, rhs: Int): Int
-}
-
-
 @Composable
 fun AppMain() {
     Column() {
